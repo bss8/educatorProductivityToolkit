896c9c8e7a410c14b2793230dc4e7a40216918b5
Boris <borislav.sabotinov@gmail.com>
2015-07-10 18:54:47 -0500

    Boris: added a test directory and some initial test classes.

---
+++ test/Controllers/AuthenticationControllerTest.java
@@ -0,0 +1,28 @@
+package Controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * AuthenticationControllerTest created by Borislav S. on 7/10/2015 @ 6:30 PM.
+ */
+public class AuthenticationControllerTest extends AuthenticationController {
+
+   @Before
+   public void setUp() throws Exception {
+
+   }
+
+   @After
+   public void tearDown() throws Exception {
+
+   }
+
+   @Test
+   public void testAuthenticateUser() throws Exception {
+
+   }
+}
---
+++ test/emailService/MailTest.java
@@ -0,0 +1,28 @@
+package emailService;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * MailTest created by Borislav S. on 7/10/2015 @ 6:31 PM.
+ */
+public class MailTest extends Mail {
+
+   @Before
+   public void setUp() throws Exception {
+
+   }
+
+   @After
+   public void tearDown() throws Exception {
+
+   }
+
+   @Test
+   public void testGenerateAndSendEmail() throws Exception {
+
+   }
+}
