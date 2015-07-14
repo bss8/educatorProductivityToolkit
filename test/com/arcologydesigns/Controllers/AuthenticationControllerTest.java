ef89a964675b5b44f72ff5327bb7cdedcd8ed4c5
bss64 <borislav.sabotinov@gmail.com>
2015-07-13 19:21:04 -0500

    Boris: adding tests

---
+++ test/com/arcologydesigns/Controllers/AuthenticationControllerTest.java
@@ -0,0 +1,31 @@
+package com.arcologydesigns.Controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * AuthenticationControllerTest created by Borislav S. on 7/10/2015 @ 6:30 PM.
+ */
+public class AuthenticationControllerTest extends AuthenticationController {
+
+   @Before
+   public void setUp() throws Exception {
+
+   }
+
+   @After
+   public void tearDown() throws Exception {
+
+   }
+
+   @Test
+   public void testAuthenticateUser() throws Exception {
+
+   }
+
+   @Test
+   public void testAuthenticateUser1() throws Exception {
+
+   }
+}
---
+++ test/com/arcologydesigns/emailService/MailTest.java
@@ -0,0 +1,26 @@
+package com.arcologydesigns.emailService;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * MailTest created by Borislav S. on 7/10/2015 @ 6:31 PM.
+ */
+public class MailTest extends Mail {
+
+   @Before
+   public void setUp() throws Exception {
+
+   }
+
+   @After
+   public void tearDown() throws Exception {
+
+   }
+
+   @Test
+   public void testGenerateAndSendEmail() throws Exception {
+
+   }
+}
