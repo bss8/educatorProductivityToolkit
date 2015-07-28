7e4057d155a97b85c10ac8b009cdaa4cd39ecbc8
ajn21 <neeley.a14@gmail.com>
2015-07-27 20:33:12 -0500

    ajn:  Student Function Test edited.

--- test/com/arcologydesigns/ept/users/StudentTest.java
+++ test/com/arcologydesigns/ept/users/StudentTest.java
@@ -1,5 +1,6 @@
 package com.arcologydesigns.ept.users;
 
+import com.sun.prism.shader.Solid_ImagePattern_Loader;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,25 +20,44 @@ public class StudentTest {
     @After
    public void tearDown() throws Exception {
 
- /*
-      This was throwing the error at the end of our call, commenting it out fixed it.
-      @After
- */
+
 
    }
 
    @Test
    public void testCompareTo() throws Exception {
+      Student Foo = new Student("Bob", 0);
+      Student Bar = new Student("", 0);
 
+      int test = Foo.compareTo(Bar);
+      System.out.printf("%s", test);
+      assert (test == 0);
+
+      Student A = new Student ( "", 1);
+      Student B = new Student ( "", 0);
+      test = A.compareTo(B);
+      assert (test == 1);
+
+      test = B.compareTo(A);
+      assert ( test == -1);
    }
 
    @Test
    public void testToString() throws Exception {
 
+      Student Foo = new Student ( "Jacob" , 94);
+      String Bar = Foo.toString();
+      assertEquals ("94.0", Bar);
+
+
    }
 
    @Test
    public void testFullStringRep() throws Exception {
+      Student J = new Student("John", 85);
+      String test = J.fullStringRep();
+      assertEquals("John: 85.0", J.fullStringRep());
+      System.out.println(test);//works fine but i'm seeing an '0' character here.
 
    }
 }
