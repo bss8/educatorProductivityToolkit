27b8ed5b8580229fdc2be0fb94cea7676dd21e22
Boris <borislav.sabotinov@gmail.com>
2015-08-03 00:15:57 -0400

    Boris: adding beginning of import data implementation.

--- src/com/arcologydesigns/GoogleIntegration/SpreadsheetIntegration.java
+++ src/com/arcologydesigns/GoogleIntegration/SpreadsheetIntegration.java
@@ -1,8 +1,13 @@
 package com.arcologydesigns.GoogleIntegration;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import static java.lang.System.*;
 
 /**
  * SpreadsheetIntegration created by Borislav S. on 7/10/2015 @ 7:06 PM.
@@ -30,21 +35,58 @@ public class SpreadsheetIntegration {
       }
    } // end class UserData
 
+   /** This default constructor is used exclusively for user authentication */
    public SpreadsheetIntegration() throws IOException {
       URL googleSpreadsheet = new URL("https://docs.google.com/spreadsheets/d/1uJVvxDvBN-pvjwuSWcTjdmX9DePW0a3y1JByMMIzpIM/pub?output=csv");
       BufferedReader in = new BufferedReader(new InputStreamReader(googleSpreadsheet.openStream()));
 
-      fetchUserData(in);
+      fetchUserLoginData(in);
    } // end SpreadSheetIntegration()
 
-   private void fetchUserData(BufferedReader in) throws IOException {
+   public SpreadsheetIntegration(String myURL, char dataType)  throws MalformedURLException, IOException {
+      URL googleSpreadsheet = null;
+
+      try {
+         googleSpreadsheet = new URL(myURL);
+      } catch (MalformedURLException e) {
+         e.printStackTrace();
+      }
+
+      try {
+         assert googleSpreadsheet != null;
+         BufferedReader readIn = new BufferedReader(new InputStreamReader(googleSpreadsheet != null ? googleSpreadsheet.openStream() : null));
+
+         switch (dataType) {
+            case 'A':
+               fetchAssignmentData(readIn);
+               break;
+            case 'I':
+               fetchInstructorData(readIn);
+               break;
+            case 'S':
+               fetchStudentData(readIn);
+               break;
+            case 'C':
+               fetchClassData(readIn);
+               break;
+            default:
+               out.printf("Invalid data type; unable to read in...");
+               break;
+         }
+
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+
+   private void fetchUserLoginData(BufferedReader in) throws IOException {
       String inputLine;
       int numRows = 0;
       in.readLine(); // skip headers
 
       while ((inputLine = in.readLine()) != null) {
 
-         System.out.printf("%s\n%n", inputLine);
+         out.printf("%s\n%n", inputLine);
          UserNode uNode = new UserNode();
 
          final String split[] = inputLine.split(",");
@@ -57,13 +99,26 @@ public class SpreadsheetIntegration {
          if(uNode.username != null && uNode.password != null) {
             userData.add(numRows, uNode);
          }
-
          numRows++;
       }
 
       UserData.setUserData(userData);
-
       in.close();
+   }  // end fetchUserLoginData()
 
-   }  // end fetchUserData()
+   private void fetchClassData(BufferedReader in) throws IOException {
+
+   }
+
+   private void fetchInstructorData(BufferedReader in) throws IOException {
+
+   }
+
+   private void fetchStudentData(BufferedReader in) throws IOException {
+
+   }
+
+   private void fetchAssignmentData(BufferedReader in) throws IOException {
+
+   }
 }  //end class SpreadsheetIntegration
