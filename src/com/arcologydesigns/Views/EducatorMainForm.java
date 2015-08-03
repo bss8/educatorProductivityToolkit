8d21c6f2b816cb71987a7acf2b700842e40a8141
Boris <borislav.sabotinov@gmail.com>
2015-08-02 21:59:01 -0400

    Boris: added two new menu items; - updated educatorForm code for style and readability.

--- src/com/arcologydesigns/Views/EducatorMainForm.java
+++ src/com/arcologydesigns/Views/EducatorMainForm.java
@@ -1,9 +1,25 @@
 package com.arcologydesigns.Views;
 
-import com.sun.javafx.scene.control.Keystroke;
+import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -28,18 +44,25 @@ public class EducatorMainForm extends JFrame {
    private JPanel userIdPanel;
    private JLabel helpText1;
    private JLabel helpText2;
-   private JTextField httpsDocsGoogleComTextField3;
-   private JTextField httpsDocsGoogleComTextField2;
-   private JTextField httpsDocsGoogleComTextField1;
-   private JTextField httpsDocsGoogleComTextField;
+   private JTextField assignmentsTextField;
+   private JTextField classesTextField;
+   private JTextField studentsTextField;
+   private JTextField instructorsTextField;
    private JLabel helpText3;
+   private JLabel instructorsLabel;
+   private JLabel studentsLabel;
+   private JLabel classesLabel;
+   private JLabel assignmentsLabel;
    private String imgUrl;
    private JPopupMenu pmenu;
 
+   /**
+    * MenuItemAction class serves to create a menu item with a label, a visual icon, and a mnemonic.
+    * Mnemonics offer a way to use the keyboard to navigate the menu hierarchy, increasing the accessibility of programs.
+    * */
    private class MenuItemAction extends AbstractAction {
 
-      public MenuItemAction(String text, Icon icon,
-                            Integer mnemonic) {
+      public MenuItemAction(String text, Icon icon, Integer mnemonic) {
          super(text);
 
          putValue(SMALL_ICON, icon);
@@ -48,7 +71,6 @@ public class EducatorMainForm extends JFrame {
 
       @Override
       public void actionPerformed(ActionEvent e) {
-
          System.out.println(e.getActionCommand());
       }
    }
@@ -110,6 +132,7 @@ public class EducatorMainForm extends JFrame {
       // Set the background, black with 125 as alpha value. This is less transparent
       if(educatorMainPanel != null) {
          educatorMainPanel.setBackground(new Color(33, 71, 116, 165));
+         mainContentPanel.setBackground(new Color(33, 71, 116, 165));
 
          // Set some size to the panels
          educatorMainPanel.setPreferredSize(new Dimension(1920, 600));
@@ -124,6 +147,9 @@ public class EducatorMainForm extends JFrame {
       setVisible(true);
    } // end createAndShowGUI()
 
+   /**
+    * This method creates a right-click context menu for the application.
+    * */
    private void createPopupMenu() {
 
       pmenu = new JPopupMenu();
@@ -132,14 +158,11 @@ public class EducatorMainForm extends JFrame {
       maxMi.setOpaque(true);
 
       maxMi.addActionListener(new ActionListener() {
-
          @Override
          public void actionPerformed(ActionEvent e) {
-
             if (getExtendedState() != JFrame.MAXIMIZED_BOTH) {
                setExtendedState(JFrame.MAXIMIZED_BOTH);
             }
-
          }
       });
 
@@ -149,7 +172,6 @@ public class EducatorMainForm extends JFrame {
       quitMi.setOpaque(true);
       quitMi.setBackground(Color.CYAN);
       quitMi.addActionListener(new ActionListener() {
-
          @Override
          public void actionPerformed(ActionEvent e) {
             System.exit(0);
@@ -164,11 +186,12 @@ public class EducatorMainForm extends JFrame {
 
       pmenu.add(quitMi);
 
+      /**
+       * Listen for a right-click event (Button3) and get the coordinates to display the popup menu
+       * */
       addMouseListener(new MouseAdapter() {
-
          @Override
          public void mouseReleased(MouseEvent e) {
-
             if (e.getButton() == MouseEvent.BUTTON3) {
                pmenu.show(e.getComponent(), e.getX(), e.getY());
             }
@@ -177,37 +200,29 @@ public class EducatorMainForm extends JFrame {
    }
 
    private void createMenuBar() {
+      //Icons for File menu
       Icon iconNew =  UIManager.getIcon("FileChooser.newFolderIcon");
       Icon iconSave =  UIManager.getIcon("FileView.floppyDriveIcon");
       Icon iconOpen =  UIManager.getIcon("Tree.timeFactor");
       Icon iconExit = UIManager.getIcon("InternalFrame.closeIcon");
 
+      //Icons for Options menu item in Tools menu
       Icon iconOptions = UIManager.getIcon("FileChooser.detailsViewIcon");
 
+      //Icons for documentation menu item in Help menu
       Icon iconHelp = UIManager.getIcon("OptionPane.questionIcon");
 
-
+      //create a menu bar for the application
       JMenuBar menubar = new JMenuBar();
 
       JMenu fileMenu = new JMenu("File");
       fileMenu.setMnemonic(KeyEvent.VK_F);
-
+      //Menu items for File menu
       JMenuItem newMi = new JMenuItem(new MenuItemAction("New", iconNew, KeyEvent.VK_N));
       JMenuItem openMi = new JMenuItem(new MenuItemAction("Open", iconOpen, KeyEvent.VK_O));
       JMenuItem saveMi = new JMenuItem(new MenuItemAction("Save", iconSave, KeyEvent.VK_S));
-
-      JMenuItem optionsMi = new JMenuItem(new MenuItemAction("Options", iconOptions, KeyEvent.VK_O));
-      JMenuItem nimbusMi = new JMenuItem(new MenuItemAction("Nimbus LaF", null, null));
-      JMenuItem metalMi = new JMenuItem(new MenuItemAction("Metal LaF", null, null));
-      JMenuItem systemMi = new JMenuItem(new MenuItemAction("System LaF", null, null));
-
-      JMenuItem documentationMi = new JMenuItem(new MenuItemAction("Documentation", iconHelp, KeyEvent.VK_H));
-      JMenuItem aboutMi = new JMenuItem("About");
-      aboutMi.setMnemonic(KeyEvent.VK_A);
-      aboutMi.setToolTipText("Learn about the EPT application and access GitHub URL");
-      aboutMi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, CTRL_MASK));
-
       JMenuItem exitMi = new JMenuItem("Exit", iconExit);
+
       exitMi.setMnemonic(KeyEvent.VK_E);
       exitMi.setToolTipText("Exit application");
       exitMi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, CTRL_MASK));
@@ -225,14 +240,44 @@ public class EducatorMainForm extends JFrame {
       fileMenu.addSeparator();
       fileMenu.add(exitMi);
 
-      JMenu viewMenu = new JMenu("View");
-      viewMenu.setMnemonic(KeyEvent.VK_V);
-
       JMenu toolsMenu = new JMenu("Tools");
+      //Menu items for Tools menu
+      JMenuItem optionsMi = new JMenuItem(new MenuItemAction("Options", iconOptions, KeyEvent.VK_O));
+      JMenuItem nimbusMi = new JMenuItem(new MenuItemAction("Nimbus LaF", null, null));
+      JMenuItem metalMi = new JMenuItem(new MenuItemAction("Metal LaF", null, null));
+      JMenuItem systemMi = new JMenuItem(new MenuItemAction("System LaF", null, null));
+
       toolsMenu.add(optionsMi);
       toolsMenu.add(nimbusMi);
       toolsMenu.add(metalMi);
       toolsMenu.add(systemMi);
+
+      JMenu helpMenu = new JMenu("Help");
+      //Menu items for Help menu
+      JMenuItem documentationMi = new JMenuItem(new MenuItemAction("Documentation", iconHelp, KeyEvent.VK_H));
+      JMenuItem aboutMi = new JMenuItem("About");
+      aboutMi.setMnemonic(KeyEvent.VK_A);
+      aboutMi.setToolTipText("Learn about the EPT application and access GitHub URL");
+      aboutMi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, CTRL_MASK));
+      JCheckBoxMenuItem sbarMi = new JCheckBoxMenuItem("Show instructions");
+      sbarMi.setMnemonic(KeyEvent.VK_S);
+      sbarMi.setDisplayedMnemonicIndex(5);
+      sbarMi.setSelected(true);
+
+      helpMenu.add(sbarMi);
+      helpMenu.add(documentationMi);
+      helpMenu.add(aboutMi);
+
+      JMenu information = new JMenu("Instructions");
+      JMenuItem currentUserInfo = new JMenuItem("Current User Info");
+      information.add(currentUserInfo);
+
+      JMenu analytics = new JMenu("Analytics");
+      JMenuItem runAnalytics = new JMenuItem("Run Analytics");
+      analytics.add(runAnalytics);
+
+      JMenu viewMenu = new JMenu("View");
+      viewMenu.setMnemonic(KeyEvent.VK_V);
 
       optionsMi.addActionListener(new ActionListener() {
          @Override
@@ -262,20 +307,9 @@ public class EducatorMainForm extends JFrame {
          }
       });
 
-      JMenu helpMenu = new JMenu("Help");
-      helpMenu.add(documentationMi);
-      helpMenu.add(aboutMi);
-
-      JCheckBoxMenuItem sbarMi = new JCheckBoxMenuItem("Show statubar");
-      sbarMi.setMnemonic(KeyEvent.VK_S);
-      sbarMi.setDisplayedMnemonicIndex(5);
-      sbarMi.setSelected(true);
-
       sbarMi.addItemListener(new ItemListener() {
-
          @Override
          public void itemStateChanged(ItemEvent e) {
-
             if (e.getStateChange() == ItemEvent.DESELECTED) {
                helpText1.setVisible(false);
                helpText2.setVisible(false);
@@ -285,15 +319,15 @@ public class EducatorMainForm extends JFrame {
                helpText2.setVisible(true);
                helpText3.setVisible(true);
             }
-
          }
-
       });
 
-      viewMenu.add(sbarMi);
+      //add menu items to the menu bar and set the menu
       menubar.add(fileMenu);
       menubar.add(viewMenu);
       menubar.add(toolsMenu);
+      menubar.add(information);
+      menubar.add(analytics);
       menubar.add(Box.createHorizontalGlue());
       menubar.add(helpMenu);
 
