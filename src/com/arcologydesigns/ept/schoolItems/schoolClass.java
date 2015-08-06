a61b0ae6c3bb216329ad5295eab34c8ab8cc651e
Boris <borislav.sabotinov@gmail.com>
2015-08-06 19:32:02 -0400

    commit

---
+++ src/com/arcologydesigns/ept/schoolItems/schoolClass.java
@@ -0,0 +1,10 @@
+package com.arcologydesigns.ept.schoolItems;
+
+import com.arcologydesigns.DataStructures.Queue;
+
+/**
+ * Created by Boris on 8/3/2015.
+ */
+public class schoolClass {
+   private Queue<Assignment> classAssignments;
+}
